8c285161161ca05cb33c3f69da87342ca1edadc2
user42 <ajuln@hotmail.fr>
2022-05-16 17:50:47 +0200

    fini

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -5,7 +5,7 @@ while ! mariadb -h$MYSQL_HOST -u$WP_DATABASE_USR -p$WP_DATABASE_PWD $WP_DATABASE
     sleep 3
 done
 
-if [ ! -f "/var/www/html/index.html" ]; then
+if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
 
     # static website
     mv /tmp/index.html /var/www/html/index.html
@@ -14,7 +14,7 @@ if [ ! -f "/var/www/html/index.html" ]; then
     wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
     wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
     wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-    wp theme install twentyfifteen --activate --allow-root
+    wp theme install twentysixteen --activate --allow-root
     wp option update blogdescription "Le blog magnifique du monde magnifique de inception pour 19 par jandre" --allow-root
 
 fi
